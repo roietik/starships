31fa23983567362fc0405f11a939775d7c896e7e
Radek <radoslaw.grzymala@hotmail.com>
2024-09-25 19:36:24 +0200

    feat: app view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GameViewComponent} from './components/game/game-view/game-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'game';
+  title = 'Game';
 }
