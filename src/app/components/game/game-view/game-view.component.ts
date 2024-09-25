4485a4ad1ee0dd2a8d10b06a7c5b35174404af30
Radek <radoslaw.grzymala@hotmail.com>
2024-09-25 19:36:11 +0200

    feat: game view

---
+++ src/app/components/game/game-view/game-view.component.ts
@@ -0,0 +1,61 @@
+import {Component} from '@angular/core';
+import {GameService} from '../../../services/game/game.service';
+import {IPerson, IStarship, TEndpoint} from '../../../services/game/game.interfaces';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import {combineLatest, finalize, Observable, switchMap, tap} from 'rxjs';
+import {GameCardsComponent} from '../game-cards/game-cards.component';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule} from '@angular/forms';
+import {MatButton, MatFabButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+
+export type IGameItem = IPerson | IStarship;
+
+@Component({
+  selector: 'game-view',
+  standalone: true,
+  templateUrl: 'game-view.component.html',
+  styleUrl: 'game-view.component.scss',
+  imports: [
+    MatSelectModule,
+    GameCardsComponent,
+    NgIf,
+    AsyncPipe,
+    FormsModule,
+    NgForOf,
+    MatButton,
+    MatIcon,
+    MatFabButton,
+    MatProgressSpinner
+  ]
+})
+export class GameViewComponent  {
+  items$?: Observable<[IGameItem, IGameItem]>;
+  selectedType: TEndpoint = 'starships';
+  type: TEndpoint = 'starships';
+  typeOptions = ['people', 'starships'];
+  loading = false;
+
+  constructor(
+    private readonly gameService: GameService
+  ) {
+  }
+
+  playGame(): void {
+    this.type = this.selectedType;
+    this.items$ = this.gameService.getItems(this.selectedType)
+      .pipe(
+        tap(() => this.loading = true),
+        switchMap((response) => {
+          return combineLatest([
+            this.gameService.getItem<IGameItem>(response[0].url),
+            this.gameService.getItem<IGameItem>(response[1].url)
+          ])
+            .pipe(
+              finalize(() => this.loading = true)
+            );
+        })
+      );
+  }
+}
