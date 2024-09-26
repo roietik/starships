26e7118e53e81df9f1366ab6d02faa8407614054
Radek <radoslaw.grzymala@hotmail.com>
2024-09-26 03:13:46 +0200

    test: should get getItem() with success

--- src/app/services/game/game.service.spec.ts
+++ src/app/services/game/game.service.spec.ts
@@ -192,6 +192,34 @@ const responseItemsMockup = {
       url: "https://www.swapi.tech/api/starships/75"
     }
   ]
+};
+const responseItemMockup = {
+  message: "ok",
+  result: {
+    properties: {
+      model: "DS-1 Orbital Battle Station",
+      starship_class: "Deep Space Mobile Battlestation",
+      manufacturer: "Imperial Department of Military Research, Sienar Fleet Systems",
+      cost_in_credits: "1000000000000",
+      length: "120000",
+      crew: "342,953",
+      passengers: "843,342",
+      max_atmosphering_speed: "n/a",
+      hyperdrive_rating: "4.0",
+      MGLT: "10",
+      cargo_capacity: "1000000000000",
+      consumables: "3 years",
+      pilots: [],
+      created: "2020-09-17T17:55:06.604Z",
+      edited: "2020-09-17T17:55:06.604Z",
+      name: "Death Star",
+      url: "https://www.swapi.tech/api/starships/9"
+    },
+    description: "A Starship",
+    _id: "5f63a34fee9fd7000499be21",
+    uid: "9",
+    __v: 0
+  }
 }
 
 describe('GameService', () => {
@@ -257,6 +285,34 @@ describe('GameService', () => {
           name: jasmine.any(String),
           url: jasmine.any(String)
         });
+      });
+    });
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should get getItem() with success', () => {
+    httpClientSpy.get.and.returnValue(of(responseItemMockup));
+    service.getItem('starships').subscribe((data) => {
+      expect(typeof data === 'object').toBe(true);
+
+      expect(data).toEqual({
+        name: jasmine.any(String),
+        model: jasmine.any(String),
+        starship_class: jasmine.any(String),
+        manufacturer: jasmine.any(String),
+        cost_in_credits: jasmine.any(String),
+        length: jasmine.any(String),
+        crew: jasmine.any(String),
+        passengers: jasmine.any(String),
+        max_atmosphering_speed: jasmine.any(String),
+        hyperdrive_rating: jasmine.any(String),
+        MGLT: jasmine.any(String),
+        cargo_capacity: jasmine.any(String),
+        consumables: jasmine.any(String),
+        pilots: jasmine.any(Array),
+        created: jasmine.any(String),
+        edited: jasmine.any(String),
+        url: jasmine.any(String),
       });
     });
     expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
